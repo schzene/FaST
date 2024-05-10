91ad1cf348483864053e1fee799d2e77e30216f2
schzene <schzene@outlook.com>
2024-05-10 13:30:41 +0000

    add multithreads

--- test/RFCP_matmul_test.cpp
+++ test/RFCP_matmul_test.cpp
@@ -1,4 +1,26 @@
 #include <model.h>
+#include <mutex>
+#include <thread>
+
+using std::thread;
+
+void for_acc(const size_t start, const size_t end,
+             const std::function<void(size_t, size_t)> &for_range, const int threads_count = 4) {
+    const size_t step = (end - start) / threads_count + 1;
+    thread *threads = new thread[threads_count];
+
+    for (int i = 0; i < threads_count; ++i) {
+        size_t thread_start = start + i * step;
+        size_t thread_end = std::min(thread_start + step, end);
+        threads[i] = thread(for_range, thread_start, thread_end);
+    }
+    for (int i = 0; i < threads_count; ++i) {
+        threads[i].join();
+    }
+
+    delete[] threads;
+}
+
 INIT_TIMER
 
 LongCiphertext RFCP_matmul(const LongCiphertext *A_secret,
@@ -14,34 +36,59 @@ LongCiphertext RFCP_matmul(const LongCiphertext *A_secret,
             Be[i * dim1 * dim3 + j] = B[i * dim3 + j % dim3];
         }
     }
-    LongPlaintext *lpt = new LongPlaintext[dim2];
-    START_TIMER
-    for (i = 0; i < dim2; i++) {
-        lpt[i] = LongPlaintext(matrix(Be.begin() + dim1 * dim3 * i, Be.begin() + dim1 * dim3 * (i + 1)), encoder);
-    }
-    STOP_TIMER("encode in function")
-    
-    LongCiphertext result = A_secret[0].multiply_plain(lpt[0], evaluator);
-    START_TIMER
+
+    LongPlaintext lpt = LongPlaintext(matrix(Be.begin(), Be.begin() + dim1 * dim3), encoder);
+    LongCiphertext result = A_secret[0].multiply_plain(lpt, evaluator);
     for (i = 1; i < dim2; i++) {
-        LongCiphertext tmp = A_secret[i].multiply_plain(lpt[i], evaluator);
+        lpt = LongPlaintext(matrix(Be.begin() + dim1 * dim3 * i, Be.begin() + dim1 * dim3 * (i + 1)), encoder);
+        LongCiphertext tmp = A_secret[i].multiply_plain(lpt, evaluator);
         result.add_inplace(tmp, evaluator);
     }
-    STOP_TIMER("compute")
-    delete[] lpt;
+    return result;
+}
+
+LongCiphertext RFCP_matmul_multi_thread(const LongCiphertext *A_secret,
+                                        const matrix &B,
+                                        size_t dim1, size_t dim2, size_t dim3,
+                                        CKKSEncoder *encoder, Evaluator *evaluator, const int thread_count = 12) {
+    // we assume that A_secret has encoded
+    size_t i, j, k;
+
+    matrix Be(dim1 * dim2 * dim3);
+    for (i = 0; i < dim2; i++) {
+        for (j = 0; j < dim1 * dim3; j++) {
+            Be[i * dim1 * dim3 + j] = B[i * dim3 + j % dim3];
+        }
+    }
+    LongPlaintext lpt = LongPlaintext(matrix(Be.begin(), Be.begin() + dim1 * dim3), encoder);
+    LongCiphertext result = A_secret[0].multiply_plain(lpt, evaluator);
+    std::mutex mtx;
+
+    auto for_range = [&Be, &dim1, &dim2, &dim3, &A_secret, evaluator, encoder, &result, &mtx](size_t start, size_t end) {
+        LongPlaintext lpt_start = LongPlaintext(matrix(Be.begin() + dim1 * dim3 * start, Be.begin() + dim1 * dim3 * (start + 1)), encoder);
+        LongCiphertext tmp1 = A_secret[start].multiply_plain(lpt_start, evaluator);
+
+        for (size_t i = start + 1; i < end; i++) {
+            LongPlaintext lpt = LongPlaintext(matrix(Be.begin() + dim1 * dim3 * i, Be.begin() + dim1 * dim3 * (i + 1)), encoder);
+            LongCiphertext tmp = A_secret[i].multiply_plain(lpt, evaluator);
+            tmp1.add_inplace(tmp, evaluator);
+        }
+
+        {
+            std::lock_guard<std::mutex> lock(mtx);
+            result.add_inplace(tmp1, evaluator);
+        }
+    };
+    for_acc(1, dim2, for_range);
     return result;
 }
 
 int main() {
-    auto step = 4;
+    auto step = 2;
     size_t dim1 = batch_size / step, dim2 = d_module / step, dim3 = ffn_dim / step, i, j;
     matrix A(dim1 * dim2), B(dim2 * dim3);
     random_mat(A);
     random_mat(B);
-    
-    START_TIMER
-    auto true_result = matmul(A, B, dim1, dim2, dim3);
-    STOP_TIMER("matmul")
 
     EncryptionParameters parms(scheme_type::ckks);
     parms.set_poly_modulus_degree(poly_modulus_degree);
@@ -57,22 +104,29 @@ int main() {
             Ae[i * dim1 * dim3 + j] = A[j / dim3 * dim2 + i];
         }
     }
-    START_TIMER
     LongCiphertext *lct = new LongCiphertext[dim2];
     for (i = 0; i < dim2; i++) {
         LongPlaintext lpt(matrix(Ae.begin() + dim1 * dim3 * i, Ae.begin() + dim1 * dim3 * (i + 1)), encoder);
         lct[i] = LongCiphertext(lpt, party);
     }
-    STOP_TIMER("encode")
 
-    START_TIMER;
-    auto result_secret = RFCP_matmul(lct, B, dim1, dim2, dim3, encoder, evaluator);
-    STOP_TIMER("RFCP_matmul")
+    // START_TIMER
+    // auto true_result_secret = RFCP_matmul(lct, B, dim1, dim2, dim3, encoder, evaluator);
+    // STOP_TIMER("RFCP_matmul")
+    // auto true_result_plain = true_result_secret.decrypt(party);
+    // auto true_result = true_result_plain.decode(encoder);
+
+    START_TIMER
+    auto result_secret = RFCP_matmul_multi_thread(lct, B, dim1, dim2, dim3, encoder, evaluator);
+    STOP_TIMER("RFCP_matmul_multi_thread")
     auto result_plain = result_secret.decrypt(party);
     auto result = result_plain.decode(encoder);
+    // for (i = 0; i < dim1 * dim3; i++) {
+    //     true_result[i] -= result[i];
+    // }
 
-    print_mat(true_result, dim1, dim3);
-    print_mat(result, dim1, dim3);
+    // std::cout << "error:\n";
+    // print_mat(true_result, dim1, dim3);
 
     delete context;
     delete encoder;
--- test/mat_test.cpp
+++ test/mat_test.cpp
@@ -1,18 +1,36 @@
-#include <model.h>
-INIT_TIMER
-int main() {
-    matrix A(batch_size, 10);
-    int step = 1000000000;
-    START_TIMER
-    double *A_data = A.data();
-    for (int i = 0; i < step; i++) {
-        A_data[i % i]; // _M_data_ptr(this->_M_impl._M_start)
-    }
-    STOP_TIMER("access by data()")
+#include <iostream>
+#include <functional>
+#include <thread>
 
-    START_TIMER
-    for (int i = 0; i < step; i++) {
-        A[i % i];
+using std::thread;
+
+void for_acc(const size_t start, const size_t end,
+             const std::function<void(size_t, size_t)> &for_range, const int threads_count = 4) {
+    const size_t step = (end - start) / threads_count + 1;
+    thread *threads = new thread[threads_count];
+
+    for (int i = 0; i < threads_count; ++i) {
+        size_t thread_start = start + i * step;
+        size_t thread_end = std::min(thread_start + step, end);
+        threads[i] = thread(for_range, thread_start, thread_end);
     }
-    STOP_TIMER("access directory")
+    for (int i = 0; i < threads_count; ++i) {
+        threads[i].join();
+    }
+
+    delete[] threads;
+}
+
+int count = 0;
+void process_range(int start, int end) {
+    for (int i = start; i < end; i++) {
+        count += i;
+    }
+}
+
+int main() {
+    const int start = 1, end = 1001;
+    for_acc(start, end, process_range, 4);
+    std::cout << count << "\n";
+    return 0;
 }
